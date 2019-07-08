cd57b0139c721e82ed01b82b32b2ff78cbb0d2b5
Syed Shahin Ahmed <syedshahinahmed@Syeds-MacBook-Pro.local>
2019-07-09 00:48:38 +0100

    App Component Updated And Successfully Getting Data From API

--- src/App.js
+++ src/App.js
@@ -2,17 +2,33 @@ import React, { Component } from "react";
 
 import "./App.css";
 import Form from "./components/Form";
+import { async } from "q";
+
+// .Edamam API ID
+const APP_ID = "ee8cf5b2";
+
+// .Edamam API KEY
+const APP_KEY = "9cc9114c66e8fda5a47697bf6aaa0032";
 
 class App extends Component {
   //Getting Recipe
-  getRecipe = e => {
+  getRecipe = async e => {
     //Preventing Default Behaviour
     e.preventDefault();
 
     //getting user input
     const recipeName = e.target.elements.recipeName.value;
 
-    console.log(recipeName);
+    // api call
+
+    const api__call = await fetch(
+      `https://api.edamam.com/search?q=${recipeName}&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+
+    // Converting Data to JSON
+    const data = await api__call.json();
+
+    console.log(data);
   };
 
   render() {
