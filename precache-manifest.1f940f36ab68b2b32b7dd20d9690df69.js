f740520b5eb881ce8a1203592e6e7891b6d50dc5
Syed Shahin Ahmed <syedshahinahmed@Syeds-MacBook-Pro.local>
2019-07-10 15:45:15 +0100

    Updates

--- precache-manifest.1f940f36ab68b2b32b7dd20d9690df69.js
+++ precache-manifest.1f940f36ab68b2b32b7dd20d9690df69.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3cb9adeb8189e3db02dc63da45af2757",
+    "revision": "9be8baf8f31863e2f32b344cf441bc2d",
     "url": "/recipe-app/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/recipe-app/static/css/2.08c8ee57.chunk.css"
   },
   {
-    "revision": "cc10addb3d535b4dd757",
+    "revision": "b897acba7272f1723eb0",
     "url": "/recipe-app/static/css/main.e162a5ef.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/recipe-app/static/js/2.bf70cf50.chunk.js"
   },
   {
-    "revision": "cc10addb3d535b4dd757",
-    "url": "/recipe-app/static/js/main.38e47fec.chunk.js"
+    "revision": "b897acba7272f1723eb0",
+    "url": "/recipe-app/static/js/main.0fc14715.chunk.js"
   },
   {
     "revision": "82c36fb6d19137cb8b75",
