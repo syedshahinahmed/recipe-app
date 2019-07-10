0108b04e1341e05ebfcf3370b699dc780cc1c619
Syed Shahin Ahmed <syedshahinahmed@Syeds-MacBook-Pro.local>
2019-07-10 15:19:35 +0100

    Hashrouter Added

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { HashRouter, Switch, Route } from "react-router-dom";
 import App from "../App";
 import Recipe from "./Recipe";
 
 const Router = () => (
-  <BrowserRouter>
+  <HashRouter>
     <Switch>
       <Route path="/" component={App} exact />
       <Route path="/recipe/:id" component={Recipe} />
     </Switch>
-  </BrowserRouter>
+  </HashRouter>
 );
 export default Router;
