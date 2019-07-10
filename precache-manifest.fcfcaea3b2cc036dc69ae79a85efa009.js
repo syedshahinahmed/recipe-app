614793e4ac687b5be0e1621ef339ce3105baadb8
Syed Shahin Ahmed <syedshahinahmed@Syeds-MacBook-Pro.local>
2019-07-10 15:16:59 +0100

    Updates

---
+++ precache-manifest.fcfcaea3b2cc036dc69ae79a85efa009.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db966e237c38f4d738cc0f5257324fff",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "02db80f0b7c6cf3690c4",
+    "url": "/recipe-app/static/css/2.08c8ee57.chunk.css"
+  },
+  {
+    "revision": "28defc8d7ea034953962",
+    "url": "/recipe-app/static/css/main.e162a5ef.chunk.css"
+  },
+  {
+    "revision": "02db80f0b7c6cf3690c4",
+    "url": "/recipe-app/static/js/2.56510f19.chunk.js"
+  },
+  {
+    "revision": "28defc8d7ea034953962",
+    "url": "/recipe-app/static/js/main.748756dc.chunk.js"
+  },
+  {
+    "revision": "82c36fb6d19137cb8b75",
+    "url": "/recipe-app/static/js/runtime~main.11ea1cab.js"
+  }
+]);
